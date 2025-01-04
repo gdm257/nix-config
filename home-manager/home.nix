459bba98d06f00c5cab3801fb63b3265e5821d4c
gdm257 <gdm257@gdm.annoaddy.com>
2025-01-05 04:02:40 +0900

    feat(home-manager): add programs

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -53,11 +53,47 @@
 
   # Add stuff for your user as you see fit:
   # programs.neovim.enable = true;
-  # home.packages = with pkgs; [ steam ];
+  home.packages = with pkgs; [
+    # DevOps
+    gitui
+
+    # File
+    fd
+
+    # Shell
+    firejail
+  ];
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
   programs.git.enable = true;
+
+  # File programs
+  programs.fzf = {
+    enable = true;
+  };
+  programs.lsd = {
+    enable = true;
+    enableAliases = true;
+  };
+
+  # Shell programs
+  programs.command-not-found = {
+    enable = true;
+    dbPath = "/nix/var/nix/profiles/per-user/root/channels/nixos/programs.sqlite";
+  };
+  programs.man.enable = true;
+  programs.zellij.enable = true;
+  programs.zoxide.enable = true;
+
+  # Text programs
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+  };
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
