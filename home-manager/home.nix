0b5df20fb7d1f1bb806b5144eab22bfd23d21e0f
gdm257 <gdm257@gdm.annoaddy.com>
2025-01-06 00:56:11 +0900

    fix(home-manager): fix home-manager options
    Home manager modules use `literalExpression` to prevent expression evaluation which is just for documentation purpose. So the example couldn't be used with `literalExpression`, rather using literal attribute set `{ ... }`

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -165,11 +165,9 @@
       "pwd"
     ];
     sessionVariables = { };
-    shellAliases = lib.literalExpression ''
-      {
-        z = "zoxide";
-      }
-    '';
+    shellAliases = {
+      z = "zoxide";
+    };
     profileExtra = "";
     initExtra = "";
     bashrcExtra = "";
@@ -180,19 +178,15 @@
     autocd = false;
     cdpath = [ ];
     dotDir = null;
-    shellAliases = lib.literalExpression ''
-      {
-        z = "zoxide";
-      }
-    '';
+    shellAliases = {
+      z = "zoxide";
+    };
     shellGlobalAliases = { };
-    dirHashes = lib.literalExpression ''
-      {
-        docs = "$HOME/Documents";
-        vids = "$HOME/Videos";
-        dl = "$HOME/Downloads";
-      }
-    '';
+    dirHashes = {
+      docs = "$HOME/Documents";
+      vids = "$HOME/Videos";
+      dl = "$HOME/Downloads";
+    };
     zprof.enable = false;
     autosuggestion = {
       enable = true;
@@ -218,38 +212,36 @@
   programs.man.enable = true;
   programs.starship = {
     enable = true;
-    settings = lib.literalExpression ''
-      {
-        add_newline = true;
-        character.success_symbol = "[>](bold green)";
-        directory.truncation_length = 0;
-        directory.use_os_path_seq = false;
-        directory.style = "bold #edc809";
-        username.style_user = "green";
-        username.style_root = "green";
-        username.format = "[\$user](\$style)";
-        username.disabled = false;
-        username.show_always = true;
-        hostname.ssh_only = false;
-        hostname.format = "[\$ssh_symbol](bold blue)[@\$hostname](green)";
-        hostname.disabled = false;
-        status.style = "red bold";
-        status.symbol = "🔴";
-        status.success_symbol = "🟢";
-        status.format = "[\$symbol\$signal_name\$maybe_int](\$style)";
-        status.map_symbol = true;
-        status.disabled = false;
-        battery.full_symbol = "🔋 ";
-        battery.charging_symbol = "⚡️ ";
-        battery.discharging_symbol = "💀 ";
-        battery.display = [
-          {
-            threshold = 50;
-            style = "bold red";
-          }
-        ];
-      }
-    '';
+    settings = {
+      add_newline = true;
+      character.success_symbol = "[>](bold green)";
+      directory.truncation_length = 0;
+      directory.use_os_path_seq = false;
+      directory.style = "bold #edc809";
+      username.style_user = "green";
+      username.style_root = "green";
+      username.format = "[\$user](\$style)";
+      username.disabled = false;
+      username.show_always = true;
+      hostname.ssh_only = false;
+      hostname.format = "[\$ssh_symbol](bold blue)[@\$hostname](green) ";
+      hostname.disabled = false;
+      status.style = "red bold";
+      status.symbol = "🔴";
+      status.success_symbol = "🟢";
+      status.format = "[\$symbol\$signal_name\$maybe_int](\$style)";
+      status.map_symbol = true;
+      status.disabled = false;
+      battery.full_symbol = "🔋 ";
+      battery.charging_symbol = "⚡️ ";
+      battery.discharging_symbol = "💀 ";
+      battery.display = [
+        {
+          threshold = 50;
+          style = "bold red";
+        }
+      ];
+    };
   };
   programs.zellij.enable = true;
   programs.zoxide.enable = true;
