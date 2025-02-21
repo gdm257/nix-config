61870e79be1ee7be7f7ee5500871a03a1878a771
gdm257 <gdm257@gdm.annoaddy.com>
2025-02-22 03:00:00 +0900

    ♻️ refactor(nixos): make home-manager as a module

---
+++ nixos/home-manager.nix
@@ -0,0 +1,19 @@
+{
+  inputs,
+  outputs,
+  globals,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    home-manager.nixosModules.home-manager
+    {
+      home-manager.useGlobalPkgs = true;
+      home-manager.useUserPackages = true;
+      home-manager.extraSpecialArgs = {inherit inputs outputs globals;};
+      home-manager.users.${globals.username} = import ./../home-manager/home.nix;
+    }
+  ];
+}
