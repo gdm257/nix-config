c185965f6c81097069642f5abeb2b814ed98376a
gdm257 <gdm257@gdm.annoaddy.com>
2025-02-22 18:13:02 +0900

    ✨ feat(nixos): cinfigure nix options

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -23,7 +23,7 @@
 
     # You can also split up your configuration and import pieces of it here:
     # ./nixpkgs.nix
-    # ./nix.nix
+    ./nix.nix
     # ./promox.nix
     # ./ssh.nix
   ];
--- nixos/nix.nix
+++ nixos/nix.nix
@@ -11,17 +11,13 @@
     flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
   in {
     settings = {
-      # Enable flakes and new 'nix' command
       experimental-features = "nix-command flakes";
-      # Opinionated: disable global registry
-      flake-registry = "";
       # Workaround for https://github.com/NixOS/nix/issues/9574
       nix-path = config.nix.nixPath;
     };
-    # Opinionated: disable channels
     channel.enable = true;
-
-    # Opinionated: make flake registry and nix path match flake inputs
+    optimise.automatic = true;
+    # Make flake registry and nix path match flake inputs
     registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
     nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
   };
