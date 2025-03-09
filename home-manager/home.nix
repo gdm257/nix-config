dabf1a494ca47e8cf4d238cc5896480bef1fd6ea
gdm257 <gdm257@gdm.annoaddy.com>
2025-03-10 00:15:48 +0900

    fix(home-manager): install home-manager by packages
    It is strange that home-manager options `programs.home-manager.enable` does not take effect in NixOS but effective for non-NixOS like CentOS. So I have to directly use `home.packages` from `nixpkgs` whose difference is just the version

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -56,7 +56,7 @@
   };
 
   # Enable home-manager and git
-  programs.home-manager.enable = true;
+  home.packages = [ pkgs.home-manager ];
   programs.git.enable = true;
 
   # Nicely reload system units when changing configs
